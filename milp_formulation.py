fdd93a5c46ca27b2822e26c64d1c7ce786930f8c
sudermn <nathan.sudermann@basf.com>
2018-12-20 13:42:26 +0100

    MILP reformulation added

---
+++ milp_formulation.py
@@ -0,0 +1,44 @@
+from pyomo.environ import *
+
+
+def solve_milp(Q, beta, BigM):
+    # Model declaration
+    model = ConcreteModel()
+    # Dimension of y
+    model.m = Q.shape[1]
+    # Index set
+    model.J = RangeSet(1, model.m)
+    # Decision variables
+    model.y = Var(model.J, domain=Reals)
+    model.u = Var(model.J, domain=NonNegativeReals)
+    model.v = Var(model.J, domain=NonNegativeReals)
+    model.b = Var(model.J, domain=Binary)
+    # objective function
+    model.obj = Objective(
+        expr=sum(model.u[j] + model.v[j] for j in model.J),
+        sense=maximize
+    )
+
+    # constraints
+
+    def lgs_constr(model, j):
+        # numpy starts to count at 0 and we want to start at 1
+        return sum(Q[j - 1][k - 1] * model.y[k] for k in model.J) + beta[j - 1] == model.u[j] - model.v[j]
+
+    def compl_constr_bigm1(model, j):
+        return model.u[j] <= BigM*model.b[j]
+
+    def compl_constr_bigm2(model, j):
+        return model.v[j] <= BigM*model.b[j]
+
+    model.lgs = Constraint(model.J, rule=lgs_constr)
+    model.compl_1 = Constraint(model.J, rule=compl_constr_bigm1)
+    model.compl_2 = Constraint(model.J, rule=compl_constr_bigm2)
+    # Solver
+    # possible choices: 'ipopt' (NLP), 'glpk' (MIP)
+    opt = SolverFactory('glpk')
+    # Solve statement
+    result_obj = opt.solve(model, tee=True)
+
+    model.pprint()
+    return result_obj
---
+++ nonlinear_formulation.py
@@ -0,0 +1,41 @@
+from pyomo.environ import *
+
+
+def solve_nonlinear(Q, beta):
+    # Model declaration
+    model = ConcreteModel()
+    # Dimension of y
+    model.m = Q.shape[1]
+    # Index set
+    model.J = RangeSet(1, model.m)
+    # Decision variables
+    model.y = Var(model.J, domain=Reals)
+    model.u = Var(model.J, domain=NonNegativeReals)
+    model.v = Var(model.J, domain=NonNegativeReals)
+    # objective function
+    model.obj = Objective(
+        expr=sum(model.u[j]+model.v[j] for j in model.J),
+        sense=maximize
+    )
+    # constraints
+
+
+    def lgs_constr(model, j):
+        # numpy starts to count at 0 and we want to start at 1
+        return sum(Q[j-1][k-1]*model.y[k] for k in model.J)+beta[j-1] == model.u[j]-model.v[j]
+
+
+    def compl_constr(model, j):
+        return model.u[j]*model.v[j] == 0
+
+
+    model.lgs = Constraint(model.J, rule=lgs_constr)
+    model.compl = Constraint(model.J, rule=compl_constr)
+    # Solver
+    # possible choices: 'ipopt' (NLP), 'glpk' (MIP)
+    opt = SolverFactory('ipopt')
+    # Solve statement
+    result_obj = opt.solve(model, tee=True)
+
+    model.pprint()
+    return result_obj
