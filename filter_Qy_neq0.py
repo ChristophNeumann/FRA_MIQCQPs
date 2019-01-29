638a1dee1d1c9068ababc19321b4b1bcce2f4714
sudermn <nathan.sudermann@basf.com>
2019-01-29 14:13:13 +0100

    Modifications according to Christophs genius ideas!

--- filter_Qy_neq0.py
+++ filter_Qy_neq0.py
@@ -1,5 +1,6 @@
 import algorithm_analysis as aa
 import os
+import numbers
 from model_information import *
 
 
@@ -7,16 +8,15 @@ def check_ynonlinearity(nonlinear_constraints, vars):
     for constr in nonlinear_constraints:
         grad = gradient_symb(constr, vars)
         for el in grad:
-            if type(el) is pyomo.core.kernel.expr_coopr3._SumExpression:
-                for arg in el._args:
-                    if arg._domain.name in int_type:
-                        return True
+            if not isinstance(el, numbers.Number):
+                    return True
     return False
 
 
 models_with_Qy_not_zero = []
 all_models = [file.split('.')[0] for file in os.listdir("testbed") if '.py' in file]
 it = 1
+
 for model_name in all_models:
     print(model_name)
     print('Model %d out of %d' % (it, len(all_models)))
@@ -26,9 +26,8 @@ for model_name in all_models:
     model_is_ynonlinear = check_ynonlinearity(nonlinear_constraints, vars)
     if model_is_ynonlinear:
         models_with_Qy_not_zero.append(model_name)
+        F = open('Models_With_Nonlinear_Y.txt', 'a')
+        F.write(model_name + '\n')
+        F.close()
     print('Up to now, %d out of %d models contain nonlinear constraints in y.' % (len(models_with_Qy_not_zero), it))
     it += 1
-F = open('Models_With_Nonlinear_Y.txt', 'w')
-for model in models_with_Qy_not_zero:
-    F.write(model+'\n')
-F.close()
