705c1c12ad2ecced4aa2e0a4abe0c3e732345088
christoph.neumann <christoph.neumann@kit.edu>
2019-03-07 17:27:45 +0100

    add bonmin script and way to extract the data of SOR in Evaluation paper.

--- globals.py
+++ globals.py
@@ -6,6 +6,6 @@ write_log = False
 feas_tol_SOR = 1e-4
 
 global delta_enlargement # We want to change this parameter from other modules
-delta_enlargement = 1-1e-4 # Enlargement parameter
+delta_enlargement = 1-1e-4
 opt_gap_bonmin = 1e-4
 
--- main.py
+++ main.py
@@ -7,11 +7,17 @@ from algorithm_analysis import *
 sys.setrecursionlimit(5000)
 
 logging.basicConfig(level=logging.WARNING)
-
-testbed = 'all_instances'
-test_instances = read_all_test_instances()
-
-for delta_enlargement in [0.5,0.75,1-1e-4]:
-    globals.delta_enlargement = delta_enlargement
-    result_SOR = run_SOR(test_instances)
-    save_obj(result_SOR,'SOR_' + testbed + "_" + str(delta_enlargement))
+RUN_SOR = False
+RUN_BONMIN = True
+if RUN_SOR:
+    testbed = 'all_instances'
+    test_instances = read_all_test_instances()
+    for delta_enlargement in [0.5,0.75,1-1e-4]:
+        globals.delta_enlargement = delta_enlargement
+        result_SOR = run_SOR(test_instances)
+        save_obj(result_SOR,'SOR_' + testbed + "_" + str(delta_enlargement))
+if RUN_BONMIN:
+    testbed = 'SOR_succ'
+    cutoff_values =np.load(r'results/vals_SOR.npy')
+    test_instances = read_test_instances(testbed)
+    run_bonmin(test_instances, cutoff_values)
